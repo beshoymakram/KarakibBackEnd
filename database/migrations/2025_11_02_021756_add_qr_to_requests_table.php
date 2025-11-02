3bafbda0607a61af76de618c177e46c537f6837c
beshoymakram <beshoy_makram@icloud.com>
2025-11-02 02:33:05 +0200

    update migration of qr_code to text

--- database/migrations/2025_11_02_021756_add_qr_to_requests_table.php
+++ database/migrations/2025_11_02_021756_add_qr_to_requests_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('requests', function (Blueprint $table) {
-            $table->string('qr_code')->unique()->nullable()->after('courier_id');
+            $table->text('qr_code')->unique()->nullable()->after('courier_id');
         });
     }
 
