e27ef8e3d5cc09e841cdc4759718d877e98069b4
beshoymakram <beshoy_makram@icloud.com>
2025-11-30 02:03:09 +0200

    Fixes

--- resources/views/emails/order-confirmation.blade.php
+++ resources/views/emails/order-confirmation.blade.php
@@ -280,7 +280,7 @@
     <div class="email-container">
         <div class="header">
             <div class="logo">
-                <img src="{{ url('https://iegypttravels.com/images/logo.png') }}" alt="Karakib Logo">
+                <img src="{{ url('https://karakib.netlify.app/logos/logo_horizontal.png') }}" alt="Karakib Logo">
             </div>
             <div class="tagline">Every small act of recycling creates a cleaner tomorrow</div>
         </div>
